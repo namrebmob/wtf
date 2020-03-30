9ab5ae2c0f1f5df2addba2df7800040f650b6e7f
bomberman <buslov.v@gmail.com>
2020-03-30 13:30:04 +0300

    add RequestEntityTooLarge error handler

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@
 
 import magic
 from flask import Flask, flash, request, redirect, render_template, url_for
-from werkzeug.exceptions import RequestEntityTooLarge
 from datetime import datetime
 
 app = Flask(__name__)
@@ -16,16 +15,14 @@ def index():
         if not request.files.get('file', None):
             flash('You did not select a file to upload', 'danger')
             return redirect(request.url)
-        try:
-            file = request.files.get('file').read(2048)
-        except RequestEntityTooLarge:
-            flash('File size too large. Limit is 16MB.', 'danger')
+        file = request.files.get('file').read(2048)
+        if file:
+            f_magic = magic.from_buffer(file)
+            flash(f'You file is {f_magic}.', 'success')
             return redirect(request.url)
-        f_magic = magic.from_buffer(file)
-        flash(f'You file is {f_magic}.', 'success')
     return render_template('layout.html', args=args)
 
 
 @app.errorhandler(413)
-def page_not_found(e):
-    return "Your error page for 413 status code", 413
+def request_entity_too_large(error):
+    return 'File Too Large', 413
